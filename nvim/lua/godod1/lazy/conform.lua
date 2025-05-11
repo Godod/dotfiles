a6d9b935bb9807240ddd02bba968a6aa1d2bf182
Tovstohan, Denys <titan074@gmail.com>
2025-05-11 18:42:18 +0300

    Adjusted conform and lua plugins

--- nvim/lua/godod1/lazy/conform.lua
+++ nvim/lua/godod1/lazy/conform.lua
@@ -1,12 +1,24 @@
 return {
-    'stevearc/conform.nvim',
-    opts = {},
-    config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-                lua = { "stylua" },
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = function(bufnr)
+					if require("conform").get_formatter_info("ruff_format", bufnr).available then
+						return { "ruff_format" }
+					else
+						return { "isort", "black" }
+					end
+				end,
+				javascrit = { "prettierd", "prettier", stop_after_first = true },
+			},
+            log_level = vim.log.levels.WARN,
+            format_after_save = {
+                lsp_format = "fallback",
+                timeout_ns = 500
             }
-        })
-    end
+		})
+	end,
 }
-
