f5b17b7a41af8bccf684f143c46ba55a0e19482a
Tovstohan, Denys <titan074@gmail.com>
2025-05-29 10:58:11 +0300

    fix: Removed signs from LSP diagnostic

--- nvim/lua/godod1/lazy/lsp.lua
+++ nvim/lua/godod1/lazy/lsp.lua
@@ -179,20 +179,20 @@ return {
 
 			-- Diagnostic Config
 			-- See :help vim.diagnostic.Opts
-			-- vim.diagnostic.config({
-			-- 	severity_sort = true,
-			-- 	float = { border = "rounded", source = "if_many" },
-			-- 	underline = { severity = vim.diagnostic.severity.ERROR },
-			-- 	signs = {
-			-- 		text = {
-			-- 			[vim.diagnostic.severity.ERROR] = "󰅚 ",
-			-- 			[vim.diagnostic.severity.WARN] = "󰀪 ",
-			-- 			[vim.diagnostic.severity.INFO] = "󰋽 ",
-			-- 			[vim.diagnostic.severity.HINT] = "󰌶 ",
-			-- 		},
-			-- 	},
-			-- 	virtual_text = false,
-			-- })
+			vim.diagnostic.config({
+				severity_sort = true,
+				float = { border = "rounded", source = "if_many" },
+				underline = { severity = vim.diagnostic.severity.ERROR },
+				-- signs = {
+				-- 	text = {
+				-- 		[vim.diagnostic.severity.ERROR] = "󰅚 ",
+				-- 		[vim.diagnostic.severity.WARN] = "󰀪 ",
+				-- 		[vim.diagnostic.severity.INFO] = "󰋽 ",
+				-- 		[vim.diagnostic.severity.HINT] = "󰌶 ",
+				-- 	},
+				-- },
+				virtual_text = false,
+			})
 
 			-- LSP servers and clients are able to communicate to each other what features they support.
 			--  By default, Neovim doesn't support everything that is in the LSP specification.
