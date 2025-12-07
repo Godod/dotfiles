14ad9165c0d9500a46419985f89cb81c77340be0
godod1 <titan074@gmail.com>
2025-12-07 14:59:32 +0200

    Update arch.sh
    Added deps from AUR and yay lib

--- installation/arch.sh
+++ installation/arch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 
 # Base packages
-sudo pacman -S git base-devel fish fisher fzf fd bat tmux neovim deno lazygit eza helix stow zoxide yazi markdown-oxide-git bash
+yay -S git base-devel fish fisher fzf fd bat tmux neovim deno lazygit eza helix stow zoxide yazi markdown-oxide-git bash
 
 # OMZ installation (we're testing fish)
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
