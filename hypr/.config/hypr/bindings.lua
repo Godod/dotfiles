bc58995955f44432586852db2d8db51a4c49e7b3
Tovstohan, Denys <titan074@gmail.com>
2026-07-20 19:24:04 +0300

    feat: Added print command

--- hypr/.config/hypr/bindings.lua
+++ hypr/.config/hypr/bindings.lua
@@ -61,3 +61,6 @@ hl.bind("XF86AudioNext",  hl.dsp.exec_cmd("playerctl next"),       { locked = tr
 hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
+
+-- Screenshot
+hl.bind("Print", hl.dsp.exec_cmd('grim - | satty -f - --copy-command wl-copy -o "~/Pictures/Screenshots/%Y%m%d_%H%M%S.png"'))
